563313921a98fa5c99914320e26971e55d938867
Cedd Burge <ceddlyburge@gmail.com>
2023-10-02 17:06:29 +0100

    Add DndContexts for drag drop and drag around

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
+import { DndContext } from "@dnd-kit/core";
 import "./App.css";
-import { Canvas } from "./Canvas";
+import { Canvas, customCollisionDetectionStrategy } from "./Canvas";
 
 function App() {
   return (
     <>
       <h1>React Canvas</h1>
-      <Canvas>
-        <div
-          style={{
-            position: "absolute",
-            transformOrigin: "top left",
-            top: "10px",
-            left: "10px",
-            backgroundColor: "red",
-          }}
-        >
-          Hard coded card
-        </div>
-      </Canvas>
+      {/* This is for dragging around the canvas */}
+      <DndContext
+        sensors={sensors}
+        onDragStart={handleDragStart} // stores the activeCard
+        onDragMove={handleDragMove} // uses doCardsCollide (see "Cards should not overlap" later)
+        onDragEnd={handleDragEnd} // uses calculateCanvasPosition, adds activeCard to children
+        collisionDetection={customCollisionDetectionStrategy()}
+      >
+        <Canvas>
+          {/* This is for dragging around the canvas */}
+          <DndContext
+            sensors={sensors}
+            onDragStart={handleDragStart} // stores the activeCard
+            onDragMove={handleDragMove} // uses doCardsCollide (see "Cards should not overlap" later), updates pixelCoordinates
+            onDragEnd={handleDragEnd} // updates position of activeCard
+          >
+            <div
+              className="card"
+              style={{
+                top: "10px",
+                left: "10px",
+                backgroundColor: "red",
+              }}
+            >
+              Hard coded card
+            </div>
+          </DndContext>
+        </Canvas>
+      </DndContext>
     </>
   );
 }
